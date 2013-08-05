00ba710c2419b593e78f959c43923906b66e48e6
Stepanyuk Eugeniya <_zh_box@mail.ru>
2013-08-05 10:35:42 +0800

    secon station

--- src/com/tictactoe/human/Player.java
+++ src/com/tictactoe/human/Player.java
@@ -26,30 +26,16 @@ public class Player {
     public int play(){
         Scanner sc       = new Scanner(System.in);
         int numberOfCell = 0;
-        boolean flag = false;
 
         System.out.println();
         System.out.println(this.name + "! Enter number of cell");
 
         //проверка ввода корректного номера ячейки
-        while (!flag)
-        {
-            //проверка на ввод числа
-            while (!sc.hasNextInt()) {
-                System.out.println("Please re-enter");
-                sc.nextLine();
-            }
-            numberOfCell = sc.nextInt();
-
-            //проверка на ввод допустимого значения ячейки
-            if(numberOfCell < 1 || numberOfCell > Field.FIELD_SIZE * Field.FIELD_SIZE)
-            {
-                System.out.println("Please re-enter");
-                continue;
-            }
-
-            flag = true;
+        while (!sc.hasNextInt()) {
+            System.out.println("Please re-enter");
+            sc.nextLine();
         }
+        numberOfCell = sc.nextInt();
 
         System.out.println("Your choice of " + numberOfCell);
         return numberOfCell;
